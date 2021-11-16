64e2929e0505e4a280a39e5c6844f1def9103169
18426HattoriRyoya <18426@toba-cmt.ac.jp>
2021-11-16 21:27:54 +0900

    コメント追加

--- tcp_client.py
+++ tcp_client.py
@@ -2,7 +2,7 @@ import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     #サーバを指定
     s.connect((' 192.168.11.58', 50007))
-    #メッセージを送る
+    #メッセージを送る!
     s.sendall(b'hello world')
     #ネットワークのバッファサイズは1024。サーバの文字列を取得
     data = s.recv(1024)
